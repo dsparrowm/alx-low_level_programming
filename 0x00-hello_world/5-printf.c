0bcc40aaf3e98b9194a49e621272535e85efd000
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-08 09:46:00 -0700

    updates

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,9 @@
  * always return 0 as success
  *
  *
-**/
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
