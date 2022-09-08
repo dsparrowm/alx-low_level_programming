558b06b0e5b131498fa26a3708d9711192ba0108
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-08 09:38:11 -0700

    updates

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * always return 0 as success
+ *
+ *
+**/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+}
