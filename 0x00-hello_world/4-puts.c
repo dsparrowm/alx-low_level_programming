1ef708ac7151380b1df02f82a560b9221b3e8418
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-08 02:52:29 -0700

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/*
+ *
+ * This function prints the below text
+ *
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
