e3b7e9333171c03eaf3628ffeb9d73e76090eef5
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-08 11:41:51 -0700

    updates

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-/*main: entry point*/
+/**
+ *
+ * Description: main is the entry point of all c programs
+ * Return: always 0 (success)
+ *
+ */
 int main(void)
 {
 	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
