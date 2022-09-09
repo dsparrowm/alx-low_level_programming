c9cbecb697ad85389fff25eb6262557860e14a33
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-09 16:15:08 -0700

    uodates

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	char c;
+	char a;
+
+	for (c = 'a'; c < 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (a = 'A'; a < 'Z'; a++)
+	{
+		putchar(a);
+	}	
+	putchar('\n');
+	return (0);
+}
