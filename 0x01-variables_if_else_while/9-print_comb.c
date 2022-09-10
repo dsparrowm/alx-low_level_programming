bd06ffafda7f420ace2c32cd208f1f8dfad1bf72
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-09 18:09:32 -0700

    updates

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: success
+ */
+int main(void)
+{
+	int x = '0';
+
+	while (x <= '9')
+	{
+		putchar(x);
+		if (x == '9')
+		{
+			putchar('\n');
+			return (0);
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x = x + 1;
+	}
+}
+
