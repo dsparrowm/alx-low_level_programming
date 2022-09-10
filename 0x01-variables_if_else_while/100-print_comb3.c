fbd3e99ef732524e0f0b9889c687b241772d6a61
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-09 18:40:40 -0700

    updates

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: success
+ */
+int main(void)
+{
+	int ten;
+	int one;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (one = ten + 1; one <= 9; one++)
+		{
+			putchar(ten + '0');
+			putchar(one + '0');
+			
+			if (ten < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
