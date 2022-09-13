017fbdf5a49d34c0b13c243b01cbb112029f9e78
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-13 09:23:14 -0700

    updates

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @x: first imteger
+ * @i: second integer
+ *
+ * Return: success
+ */
+int add(int x, int i)
+{
+	int z = x + i;
+
+	return (z);
+}
