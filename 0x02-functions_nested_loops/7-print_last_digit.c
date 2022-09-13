dff1350626f940dc9883d2a87aea5facc7cc7598
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-13 08:52:42 -0700

    updates

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: to be printed
+ *
+ * Return: always success
+ */
+int print_last_digit(int n)
+{
+	int x = n % 10;
+	return (x);
+}
