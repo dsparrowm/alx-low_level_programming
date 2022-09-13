80d7ce2a04923921dee5148d0282ba90c7be7180
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-13 03:45:33 -0700

    updates

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
