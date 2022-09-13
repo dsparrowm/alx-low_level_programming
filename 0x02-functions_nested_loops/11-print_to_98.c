ae56ef68f53c446f64c6dba17176ca8f445c4e8a
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-13 10:00:35 -0700

    updates

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - primts natural number
+ * @n: number to print
+ *
+ * Return: success
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n == 98)
+			{
+				printf('\n');
+				return (0);
+			}
+			printf(", ");
+			n++;
+		}
+	}
+	else if (n == 98)
+	{
+		printf("%d\n", n);
+		return (0);
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n == 98)
+			{
+				printf('\n');
+				return (0);
+			}
+			printf(", ");
+			n--;
+		}
+}
