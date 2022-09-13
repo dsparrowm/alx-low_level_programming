f7287c737ee42a6211b11910ddabdba37e4a2f71
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-13 08:01:11 -0700

    updates

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _islower - checks if a character is lowercase
+ *
+ * Return: always o for success
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
