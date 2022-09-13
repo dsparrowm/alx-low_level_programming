9f8fa124458361b740b91e51fa10a861a91890a3
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-13 07:47:46 -0700

    updates

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints a to z 10 times on screen
+ *
+ * Return o as success
+ */
+void print_alphabet_x10(void)
+{
+	int x = 0;
+
+	while (x <= 10)
+	{
+		int i = 'a';
+
+		while (i <= 'z')
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
