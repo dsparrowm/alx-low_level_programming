114c439fdf204abc93c4dabdcaf54eb829effa5b
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-14 23:57:34 -0700

    updates

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper  - checks for uppercase letter
+ * @c: character to be checked
+ *
+ * Return: 0
+ */
+ int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this project focuses on more functions and nested loops
