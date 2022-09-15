055115860589e68e7b4fd197b8dcdf6af3a9eeaf
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-15 14:48:32 -0700

    updates

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,11 +8,11 @@
  */
 void print_line(int n)
 {
-	int i = '0';
+	int i = 0;
 
-	for (i = '0'; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		if (n == '0' || n < '0')
+		if (n == 0 || n < 0)
 		{
 			_putchar('\n');
 		}
