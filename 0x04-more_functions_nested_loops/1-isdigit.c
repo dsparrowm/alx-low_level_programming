32d45e5ad52677edae5b568b94ce1c696fa85806
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-15 12:44:04 -0700

    updates

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is digit
+ * @c: character to be checked
+ *
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
