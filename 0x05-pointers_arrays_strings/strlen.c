b0fa6664e8b504a99e92d6d3945f4e627540d8a6
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-19 13:51:39 -0700

    updates

---
+++ 0x05-pointers_arrays_strings/strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - checks the length of a string
+ * @s: string to check
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	x = strlen(*s);
+	return (x);
+}
