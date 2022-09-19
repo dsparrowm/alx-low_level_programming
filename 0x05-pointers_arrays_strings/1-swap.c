869a3a7cc52a126485dc9fe3f08905927b3c8326
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-19 11:41:01 -0700

    updates

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two integers using pointers
+ * @a: first integer
+ * @b: srcond integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
