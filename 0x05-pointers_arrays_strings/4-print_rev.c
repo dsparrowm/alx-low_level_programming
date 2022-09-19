4451315624930690e5755ba33fca9a14a11566b8
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-19 14:39:14 -0700

    updates

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#imclude <stdio.h>
+
+/**
+ * print_rev - reverse a string
+ * @s: string to reverse
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	puts(strrev(s));
+}
