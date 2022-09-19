27809d277d5daf08689fa8f002f5c9db05241ff5
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-19 11:31:18 -0700

    updates

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets an integer to 98
+ * @*n: the integer to reset
+ *
+ * Retirn: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This projects focuses on pointers, arrays and strings
