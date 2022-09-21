6a98d503b56c3b5769420676118f01b708daaff6
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-21 15:28:05 -0700

    updates

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,5 +10,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	return (strncmp(s1, s2));
+	int i;
+	int R;
+
+	i = 0;
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		i++;
+	}
+	R = s1[i] - s2[i];
+	return (R);
 }
