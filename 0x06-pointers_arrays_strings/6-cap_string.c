a5dc57f63a72fb010017e183bd372e039006a794
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-22 12:04:28 -0700

    updates

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * cap_string - capitalize all words
  * @s: string to work on
@@ -8,18 +8,15 @@
  */
 char *cap_string(char *s)
 {
-	int i;
-	
-	if (s[0] >= 'a' && s[0] <= 'z')
+	int i, j;
+	char delimeters[] = " \t\n,;.!?\"(){}";
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		s[0] = s[0] -  32;
-	}
-	for (; s[i]; i++)
-	{
-		if ((s[i - 1] == '\t' || s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' || s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}' && s[i] >= 'a' && s[i] <= 'z'))
-		{
-			s[i] = s[i] -  32;
-		}
+		if (s[0] >= 97 && s[0] <= 122)
+			s[0] = s[0] - 32;
+		for (j = 0; delimeters[j] != '\0'; j++)
+			if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
 	}
 	return (s);
 }
