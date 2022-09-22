619562c4d545d2efd6ab22688d735c68b462645a
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-22 12:22:41 -0700

    updates

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * reverse_array - reverse
  * @a: array
@@ -13,8 +12,8 @@ void reverse_array(int *a, int n)
 	int j;
 	int tmp;
 	i = 0;
-
 	j = n - 1;
+
 	while (i < j)
 	{
 		tmp = a[i];
