d52fdfb53b424dc35ed1ea076d982d146ba5a711
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-22 02:57:47 -0700

    updates

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,7 +8,7 @@
 char *string_toupper(char *str)
 {
 	int i;
-	
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
@@ -16,5 +16,6 @@ char *string_toupper(char *str)
 		       str[i] = str[i] - 32;
 		}
 	}
+
 	return (str);
 }
