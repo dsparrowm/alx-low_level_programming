007f43ad08119225f890259550c7b0b6ae89fa3c
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-28 09:17:51 -0700

    updates

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _pow_recursion - prints the number to the power of another number
+ * @x: first number
+ * @y: second number
+ *
+ * Return: the power of x to y
+ */
+int _pow_recursion(int x, int y)
+{
+	return (pow(x, y));
+}
