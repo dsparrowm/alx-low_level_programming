7416f9cbf1ad465ea415ae5a5d930738b7afe5c4
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-30 14:24:34 -0700

    updates

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 /**
  * main - entry point for my program
  * @argc: number of arguments passed by the user at the command line
@@ -9,7 +8,6 @@
  */
 int main(__attribute__((unused))int argc, char *argv[])
 {
-	printf("%s\n", argv[argc * 0]);
-
+	printf("%s\n", argv[0]);
 	return (0);
 }
