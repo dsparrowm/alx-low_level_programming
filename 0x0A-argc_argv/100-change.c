691581f0537888bb0ca474c95b0009e9900f6ae5
Davies Okon <daviesaniefiok32@gmail.com>
2022-09-30 18:20:54 -0700

    updates

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,29 +3,27 @@
 /**
  * isInteger - checks if s is an integer
  * @s: string to check
+ *
  * Return: 0 or 1
  */
-
 int isInteger(const char *s)
 {
-
 	int i = 0;
 	while (s[i] != '\0')
 	{
 		if (s[i] < '0' || s[i] > '9')
 			return (0);
 		i++;
-	}	
+	}
 	return (1);
 }
-
 /**
  * main - adds positive numbers
  * @argc: int
  * @argv: list
+ *
  * Return: 0
  */
-
 int main(int argc, char const *argv[])
 {
 	int i = 0, coinUsed = 0, coin = 0;
@@ -52,8 +50,6 @@ int main(int argc, char const *argv[])
 			}
 		}
 	}
-
 	printf("%i\n", coinUsed);
-
 	return (0);
 }
