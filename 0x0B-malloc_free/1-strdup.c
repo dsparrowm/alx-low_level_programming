77c9250b800f1e052618d8410d7a156c27c381ba
Davies Okon <daviesaniefiok32@gmail.com>
2022-10-03 07:42:15 -0700

    updates

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strdup - copies another string and returns a pointer
+ * @str: string
+ *
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	char *str2;
+
+	str2 = strdup(str);
+	if (str2 == NULL)
+		return (NULL);
+	else
+		return (str2);
+}
