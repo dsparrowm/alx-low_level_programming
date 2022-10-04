a3ae9bd5e1e60542128e6c01f6fbdef699047b70
Davies Okon <daviesaniefiok32@gmail.com>
2022-10-04 00:53:20 -0700

    updates

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,12 +15,12 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 
 	if (s1 == NULL)
-		return (NULL);
+		s1 = "";
 	if (s2 == NULL)
-		return (NULL);
+		s2 = "";
 	s1_len = strlen(s1);
 	s2_len = strlen(s2);
-	s3 = (char *)malloc(sizeof(char) * (s1_len + s2_len));
+	s3 = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (s3 == NULL)
 		return (NULL);
 	strcpy(s3, s1);
