46efb0bfb1bd3d22f4bd5e96551c6a8d2bc07fdb
Davies Okon <daviesaniefiok32@gmail.com>
2022-10-10 09:59:38 -0700

    updates

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,8 +17,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	new_dog->name = malloc(sizeof(char) * strlen(name) + 1);
 	if (new_dog->name == NULL)
+	{
+		free(new_dog);
 		return (NULL);
+	}
 	new_dog->owner = malloc(sizeof(char) * strlen(owner) + 1);
+	if (new_dog->owner == NULL)
+		return (NULL);
 	strcpy(new_dog->name, name);
 	strcpy(new_dog->owner, owner);
 	new_dog->age = age;
