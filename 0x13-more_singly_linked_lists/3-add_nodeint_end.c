405ab3e30950cd1d023fe64a1c82d9d07f2973db
Davies Okon <daviesaniefiok32@gmail.com>
2022-10-24 13:53:50 -0700

    fixed error

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -26,7 +26,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		{
 			current = current->next;
 		}
+		current->next = new_node;
 	}
-	current->next = new_node;
 	return (new_node);
 }
