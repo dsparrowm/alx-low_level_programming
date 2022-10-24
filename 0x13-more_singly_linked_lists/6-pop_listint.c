9a15986b686d53e8bb75df572ba18b5c44f61a86
Davies Okon <daviesaniefiok32@gmail.com>
2022-10-24 14:47:42 -0700

    added function that deletes the head of the list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head
+ * @head: pointer to the first element
+ *
+ * Return: the head's node data(int n)
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int num;
+
+	if (head == NULL)
+		return (0);
+
+	temp = *head;
+	num = (*head)->n;
+	*head = (*head)->next;
+	free(temp);
+
+	return (num);
+}
