9e1dca3ed9a7383f9b939bd935a0838744fc302c
Davies Okon <daviesaniefiok32@gmail.com>
2022-10-24 08:55:17 -0700

    added function that frees list and sets head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list and sets head to null
+ * @head: the list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+	*head = NULL;
+}
