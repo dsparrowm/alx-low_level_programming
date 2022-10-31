cfdffc9cdc3534e636f6b0f6039e40f77dc597ca
Davies Okon <daviesaniefiok32@gmail.com>
2022-10-31 06:21:20 -0700

    fixed a bug

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,19 +8,28 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	int sx, wt;
-	const char *str[256];
+	ssize_t sx, wt;
+	char *str;
 
 	if (filename == NULL)
 		return (0);
+	str = malloc(sizeof(char *) * letters);
+	if (str == NULL)
+		return (0);
 	fd = open("filename", O_RDONLY);
 	if (fd < 0)
+	{
+		free(str);
 		return (0);
+	}
 	sx = read(fd, str, letters);
 	if (sx < 0)
+	{
+		free(str)
 		return (0);
-	wt = write(fd, str, letters);
+	}
+	wt = write(STDOUT_FILENO, str, sx);
 	if (wt < 0)
 		return (0);
-	return (strlen(filename));
+	return (wt);
 }
